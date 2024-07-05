43d80d9d3edaff5e3874366d5ea4b92c297c7ac3
omkar-nanware <nanwareom22@gmail.com>
2024-07-05 15:22:58 +0530

    Initial Commit

---
+++ distinctArray.js
@@ -0,0 +1,20 @@
+function distinctArray(arr) {
+    
+    let obj ={}
+    for(let i=0;i<arr.length;i++) {
+        obj[arr[i]] = (obj[arr[i]] || 0) + 1   
+    }
+    let newArr = []
+    
+    console.log(obj)
+    for(const key in obj) {
+        if(obj[key] === 1) {
+            newArr.push(key)
+        }
+    }
+    
+    return newArr
+   
+}
+
+console.log(distinctArray([1,2,3,3,4,5,4,5,6]))
